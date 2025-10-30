efc74c259fbb8a08bbf19a77cd6ac7d9b96a4518
BADSHAH-7868 <jodoffcl2@@example.com>
2025-10-29 22:37:07 -0500

    🌀 Auto commit: LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -73,7 +73,7 @@ export const LandingPage = ({ onProductClick, onViewProducts }: LandingPageProps
               Explore Products
               <ArrowRight className="ml-2 group-hover:translate-x-1 transition-transform" size={18} />
             </button>
-            <a href="https://www.googel.com/" className="bg-white dark:bg-slate-800 text-indigo-600 dark:text-indigo-400 border-2 border-indigo-600 dark:border-indigo-500 px-6 sm:px-8 py-3 sm:py-4 rounded-full font-semibold hover:bg-indigo-50 dark:hover:bg-slate-700 transition-all w-full sm:w-auto">></a>
+            <a href="https://www.googel.com/" className="bg-white dark:bg-slate-800 text-indigo-600 dark:text-indigo-400 border-2 border-indigo-600 dark:border-indigo-500 px-6 sm:px-8 py-3 sm:py-4 rounded-full font-semibold hover:bg-indigo-50 dark:hover:bg-slate-700 transition-all w-full sm:w-auto"></a>
             <button className="bg-white dark:bg-slate-800 text-indigo-600 dark:text-indigo-400 border-2 border-indigo-600 dark:border-indigo-500 px-6 sm:px-8 py-3 sm:py-4 rounded-full font-semibold hover:bg-indigo-50 dark:hover:bg-slate-700 transition-all w-full sm:w-auto">
               Join Channel
             </button>
